49df7ca9c0d2723f95582553ec880aa7e40f059f
wangkaiwd <1162486176@qq.com>
2018-10-23 16:52:06 +0800

    feat: 切换到新路由，页面滚动到浏览器顶部

--- src/router/index.js
+++ src/router/index.js
@@ -8,5 +8,9 @@ export default new Router({
   routes: [
     { path: '/', exact: true, name: 'home', component: Home },
     { path: '/mine', name: 'mine', component: Mine }
-  ]
+  ],
+  // 切换到新路由后时，页面滚动到顶部
+  scrollBehavior() {
+    return { x: 0, y: 0 }
+  }
 })
