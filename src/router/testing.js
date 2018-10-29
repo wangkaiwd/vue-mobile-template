6bf682a7caf1233cedb0a3d7e76f24f7586d20d3
wangkaiwd <1162486176@qq.com>
2018-10-29 09:18:02 +0800

    optimize: 优化目录结构

--- src/router/testing.js
+++ src/router/testing.js
@@ -1,4 +1,4 @@
-import { getComponent } from '@/utils/misc'
+import { getComponent } from './lazyComponent'
 export default [
   { path: '/', exact: true, name: 'home', component: getComponent('home') },
   { path: '/mine', name: 'mine', component: getComponent('mine') }
