b937d326820793ec18b6619693454adba0b1753b
wangkaiwd <1162486176@qq.com>
2018-10-29 23:09:32 +0800

    faat: toast组件完成，并添加消失动画，但是感觉有问题

--- src/components/plugin.js
+++ src/components/plugin.js
@@ -1,20 +1,30 @@
 import Toast from './myToast'
-const install = (Vue, options) => {
-  Vue.prototype.$message = function(message) {
-    // Vue.extend: 使用Vue基础构造器，创建一个子类。参数是一个包含组件选项的对象
-    const Constructor = Vue.extend(Toast)
-    // 这里重新生成一个Vue实例的原因：要将toast进行重新挂载
-    const toast = new Constructor({
-      propsData: {
-        message
-      }
-    })
-    // 如果通过lslot插槽进行toast插入的话，需要通过为组件的slot属性动态赋值
-    // toast.$slots.default = [message]
-    toast.$mount()
-    // vm.$el: Vue实例使用的根dom元素
-    document.body.appendChild(toast.$el)
+let currentToast
+const install = Vue => {
+  Vue.prototype.$message = function(message, options) {
+    if (currentToast) {
+      currentToast.close()
+    }
+    currentToast = createToast(message, options, Vue)
   }
+}
+// 创建toast实例，将toast实例添加到浏览器，并返回toast
+const createToast = (message, options, Vue) => {
+  // Vue.extend: 使用Vue基础构造器，创建一个子类。参数是一个包含组件选项的对象
+  const Constructor = Vue.extend(Toast)
+  // 这里重新生成一个Vue实例的原因：要将toast进行重新挂载
+  const toast = new Constructor({
+    propsData: {
+      message,
+      ...options
+    }
+  })
+  // 如果通过lslot插槽进行toast插入的话，需要通过为组件的slot属性动态赋值
+  // toast.$slots.default = [message]
+  toast.$mount()
+  // vm.$el: Vue实例使用的根dom元素
+  document.body.appendChild(toast.$el)
+  return toast
 }
 export default {
   install
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -2,6 +2,9 @@
   <div class="mine">
     我的
     <router-link to='/'>home</router-link>
+    <button @click="handleClick">上</button>
+    <button @click="handleClick1">中</button>
+    <button @click="handleClick2">下</button>
   </div>
 </template>
 
@@ -11,8 +14,36 @@ console.log('global', LENGTH) // 这段代码只会执行一次
 export default {
 	name: 'VMine',
 	mounted() {
-		this.$message('I am toast')
+		// this.$message('I am toast')
 		console.log('mounted', LENGTH) // mounted钩子函数会在每次页面渲染的时候执行
+	},
+	methods: {
+		handleClick() {
+			this.$message('I want to go sky', {
+				autoClose: false,
+				closeButton: {
+					text: '关闭'
+				}
+			})
+		},
+    handleClick1() {
+			this.$message('I want to go sky', {
+				autoClose: false,
+        position: 'middle',
+				closeButton: {
+					text: '关闭'
+				}
+			})
+		},
+    handleClick2() {
+			this.$message('I want to go sky', {
+				autoClose: false,
+        position: 'bottom',
+				closeButton: {
+					text: '关闭'
+				}
+			})
+		}
 	}
 }
 </script>
