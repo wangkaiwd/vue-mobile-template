ac1dd7ac72627175d82286567ae1c10ed7979ee0
wangkaiwd <1162486176@qq.com>
2018-10-29 20:34:19 +0800

    feat: 表单验证优化

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -31,7 +31,7 @@ export default {
 		validatorFunc() {
 			const validator = new FormValidator()
 			validator.add(this.userName, ['isNonEmpty', 'minLength:6'], '用户名')
-			validator.add(this.password, { rule: 'isNonEmpty' }, '密码')
+			validator.add(this.password, 'isNonEmpty', '密码')
 			validator.add(
 				this.checkCode,
 				[{ rule: 'isNonEmpty' }, { rule: 'maxLength:6' }],
