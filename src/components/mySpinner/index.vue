b89422ef012f7fea6015c94934a76f41e4d36359
wangkaiwd <1162486176@qq.com>
2018-10-31 15:08:10 +0800

    style: toast组件样式优化

---
+++ src/components/mySpinner/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="my-spinner">
+    spinner
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MySpinner",
+  data() {
+    return {};
+  },
+  components: {},
+  computed: {},
+  mounted() {},
+  methods: {}
+};
+</script>
+<style lang="less" scoped>
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -16,49 +16,49 @@
 </template>
 
 <script>
-import FormValidator from '@/utils/formValidator'
-console.log('home')
+import FormValidator from "@/utils/formValidator";
+console.log("home");
 export default {
-	name: 'VHome',
-	data() {
-		return {
-			userName: '',
-			password: '',
-			checkCode: ''
-		}
-	},
-	methods: {
-		validatorFunc() {
-			const validator = new FormValidator()
+  name: "VHome",
+  data() {
+    return {
+      userName: "",
+      password: "",
+      checkCode: ""
+    };
+  },
+  methods: {
+    validatorFunc() {
+      const validator = new FormValidator();
 
-			validator.add(this.userName, ['isNonEmpty', 'minLength:6'], '用户名')
-			validator.add(this.password, 'isNonEmpty', '密码')
-			validator.add(
-				this.checkCode,
-				[{ rule: 'isNonEmpty' }, { rule: 'maxLength:6' }],
-				'haha'
-			)
-			const errorMsg = validator.start()
-			return errorMsg
-		},
-		onSubmit(e) {
-			const errorMsg = this.validatorFunc()
-			if (errorMsg) {
-				alert(errorMsg)
-				return
-			}
-			console.log('success')
-			// 发起请求，进行接口层面的校验
-		}
-	}
-}
+      validator.add(this.userName, ["isNonEmpty", "minLength:6"], "用户名");
+      validator.add(this.password, "isNonEmpty", "密码");
+      validator.add(
+        this.checkCode,
+        [{ rule: "isNonEmpty" }, { rule: "maxLength:6" }],
+        "haha"
+      );
+      const errorMsg = validator.start();
+      return errorMsg;
+    },
+    onSubmit(e) {
+      const errorMsg = this.validatorFunc();
+      if (errorMsg) {
+        this.$message(errorMsg, { autoClose: false });
+        return;
+      }
+      console.log("success");
+      // 发起请求，进行接口层面的校验
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
 .input-wrapper {
-	padding: 10px;
-	input {
-		margin: 10px;
-	}
+  padding: 10px;
+  input {
+    margin: 10px;
+  }
 }
 </style>
